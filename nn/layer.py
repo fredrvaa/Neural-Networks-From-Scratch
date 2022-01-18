db16813e13bca5ac6363fb4f69923a18ffa17bc7
Fredrik <fredrvaa@stud.ntnu.no>
2022-01-18 14:33:17 +0100

    Implement basic forward pass for layer

---
+++ nn/layer.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+class Layer():
+    def __init__(self, input_size, output_size):
+        self.W = np.random.rand(input_size, output_size)
+        self.b = np.random.rand(output_size)
+
+    def Relu(self, z):
+        return max(0, z)
+
+    def forward_pass(self, X):
+        return np.dot(X, self.W) + self.b
+
+if __name__ == '__main__':
+    np.random.seed(0)
+    l = Layer(3,2)
+    print(l.forward_pass(np.array([1,1,1])))
